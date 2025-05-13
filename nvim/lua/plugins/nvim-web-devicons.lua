16a797786095b14ae02664c7f073d8897b4f9e4a
Conner Ow <conner@ironclad.sh>
2025-05-13 07:46:02 -0500

    fix css icon

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,3 +1,14 @@
 return {
   "nvim-tree/nvim-web-devicons",
+  opts = {
+    override = {
+      -- For some reason, CSS in particular wasn't working properly
+      css = {
+        icon = "",
+        cterm_color = "75",
+        color = "#42a5f5",
+        name = "Css"
+      }
+    }
+  }
 }
