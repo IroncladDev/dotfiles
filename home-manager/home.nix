a6873b50f224491502ae2ddbee523897a8ec73ca
IroncladDev <conner@ironclad.sh>
2026-09-11 11:28:07 -0500

    tailscale cli

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -49,6 +49,7 @@
             typescript-language-server
             vscode-langservers-extracted
             lsof
+            tailscale 
         ]
         # Linux-only
         ++ lib.optionals pkgs.stdenv.isLinux [
