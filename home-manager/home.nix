8ba041ca3a443d470529222cbe750af6b3472959
IroncladDev <conner@ironclad.sh>
2026-09-16 08:37:58 -0500

    autossh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -50,6 +50,7 @@
             vscode-langservers-extracted
             lsof
             iroh-ssh
+            autossh
         ]
         # Linux-only
         ++ lib.optionals pkgs.stdenv.isLinux [
